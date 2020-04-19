b69e5f468df4cf7377ae0f2d88f5e7fd426c599a
shahzadujan <63977748+shahzadujan@users.noreply.github.com>
2020-04-20 01:27:49 +0500

    Project Added
    Source Code Added

---
+++ ADODotnetwithASPDotnetCore/Controllers/ValuesController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using ADODotnetwithASPDotnetCore.DB;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace ADODotnetwithASPDotnetCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+Database db = new Database();
+
+[HttpPost]
+public ActionResult Post(object value)
+{
+    var serialize = JsonConvert.SerializeObject(value);
+    JObject jobject = JObject.Parse(serialize);         
+    string query = "insert into Employee (FirstName,LastName,Age) values (@FirstName,@LastName,@Age);";
+    var parameters = new IDataParameter[]
+    {
+        new SqlParameter("@FirstName", jobject["FirstName"].ToString()),
+        new SqlParameter("@LastName", jobject["LastName"].ToString()),
+        new SqlParameter("@Age",jobject["Age"].ToString())
+   };
+    if (db.ExecuteData(query,parameters) > 0)
+    {
+
+        return Ok(new { Result = "Saved" });
+    }
+    else
+    {
+        return NotFound(new { Result = "something went wrong" });
+
+    }
+}
+
+[HttpGet]
+public ActionResult<IEnumerable<string>> Get()
+{
+
+    string query = "select * from Employee";
+    DataTable dt = db.GetData(query);
+    var result = new ObjectResult(dt);
+    return result;
+}
+
+      
+
+      
+
+    }
+}
---
+++ ADODotnetwithASPDotnetCore/DB/Database.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ADODotnetwithASPDotnetCore.DB
+{
+    public class Database
+    {
+
+public static string sqlDataSource = "Data Source=DESKTOP-GV4424J;Initial Catalog=TestDB ;Integrated Security=True;";
+
+public DataTable GetData(string str)
+{
+    DataTable objresutl = new DataTable();
+    try
+    {
+        SqlDataReader myReader;
+
+        using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+        {
+
+            myCon.Open();
+            using (SqlCommand myCommand = new SqlCommand(str, myCon))
+            {
+                myReader = myCommand.ExecuteReader();
+
+
+                objresutl.Load(myReader);
+
+
+
+                myReader.Close();
+                myCon.Close();
+            }
+        }
+    }
+    catch (Exception ex)
+    {
+
+    }
+
+    return objresutl;
+
+
+}
+public int ExecuteData(string str, params IDataParameter[] sqlParams)
+{
+    int rows = -1;
+    try
+    {
+
+        using (SqlConnection conn = new SqlConnection(sqlDataSource))
+        {
+            conn.Open();
+            using (SqlCommand cmd = new SqlCommand(str, conn))
+            {
+                if (sqlParams != null)
+                {
+                    foreach (IDataParameter para in sqlParams)
+                    {
+                        cmd.Parameters.Add(para);
+                    }
+                            rows = cmd.ExecuteNonQuery();
+             }
+                 
+
+             
+
+
+            }
+        }
+    }
+    catch (Exception ex)
+    {
+
+    }
+
+
+    return rows;
+
+
+}
+    }
+}
